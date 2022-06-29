5b99beb2e7f7fd1c81976e68741a77530c1e58cb
amatProject <amatproject2022@gmail.com>
2022-06-29 17:53:55 +0300

    second commit

---
+++ extractImages.py
@@ -0,0 +1,59 @@
+import numpy as np
+from PIL import Image
+import os
+import pandas as pd
+# import imageio
+# import cv2
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict    #return a dict that has the data-images labels and names
+
+def divide_data(data):#divide batch into labels, data and names
+    labels=data[b'labels']
+    images=data[b'data']
+    names=data[b'filenames']
+    return labels,images,names
+
+def add_to_CSV(images,labels,names,batch):
+    df = {'image': images, 'labels':labels,'names':names,'batch':batch}
+    df=pd.DataFrame(data=df)
+    df.to_csv(r'../CIFAR-10.csv',mode='a')
+
+
+def save_images(images,names):
+    images_path=[]
+    path = os.path.join('../', 'images')
+    os.mkdir(path)# create a folder for the images
+
+    images = np.reshape(images, (60000, 3, 32, 32))
+    for image,image_name in zip(images,names):
+        fixed_image = np.transpose(image, (1, 2, 0))
+        fixed_image = Image.fromarray(fixed_image)
+        path='../images/'+image_name.decode("utf-8")
+        fixed_image.save(path)
+        images_path.append(path)
+    return images_path
+
+
+def load_cifar10_data_into_CSV(directory):
+    images=[]
+    labels=[]
+    names=[]
+    batch=[]
+    for filename in os.listdir(directory):
+        if filename.startswith("data_batch") or filename.startswith("test_batch"):
+            path = os.path.join(directory,  filename)  # Adding file name at the end of the address
+            data=unpickle(path)
+            batch_labels,batch_images,batch_names=divide_data(data)
+            labels+=batch_labels
+            images = [*images, *batch_images]
+            names+=batch_names
+            batch+=[filename]*len(batch_labels)
+
+    image_path=save_images(images,names)
+    # im = Image.open(image_path[1])
+    # im.show()
+    add_to_CSV(image_path,labels,names,batch)
--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-import numpy as np
-from PIL import Image
-import imageio
-import cv2
-from keras.datasets.cifar import load_batch
-from keras.utils.data_utils import get_file
-# import tensorflow
+import pandas as pd
 
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-
-array1=unpickle(r'C:\amatProject\cifar-10-batches-py\data_batch_1')
-array=array1[b'data']
-
-array
-ims=np.reshape(array,(10000,3,32,32))
-# len(ims[0][0][0])
-array1=array1[b'labels'][1]
-array1
-imgg=ims[537,:,:,:]
-imgg=np.transpose(imgg,(1,2,0))
-print(imgg.shape)
-img=Image.fromarray(imgg)
-img.show()
-
-
-def load_data(path):
-    print("vnb")
-#first commit
-#com2g
+import extractImages
+extractImages.load_cifar10_data_into_CSV(r'C:\amatProject\cifar-10-batches-py')
+df = {'image': [23526,7458], 'labels': ['fb','fhmn '],'names': [5,3], 'batch':[00,1]}
+df = pd.DataFrame(data=df)
+df.to_csv(r'../CIFAR-10.csv',mode='a')
