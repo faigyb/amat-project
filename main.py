b189881e8de11ae9e754e1d7b948a8b220684d24
amatProject <amatproject2022@gmail.com>
2022-06-30 18:50:29 +0300

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
 import extractImages
-extractImages.load_cifar10_data_into_CSV(r'C:\amatProject\cifar-10-batches-py')
-df = {'image': [23526,7458], 'labels': ['fb','fhmn '],'names': [5,3], 'batch':[00,1]}
-df = pd.DataFrame(data=df)
-df.to_csv(r'../CIFAR-10.csv',mode='a')
+extractImages.load_all_data(r'C:\amatProject')
+
