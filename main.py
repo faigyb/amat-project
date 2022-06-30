df5a9b7876bde9de343018ba3779ed873a296693
RaShahor <raracheli5400@gmail.com>
2022-06-30 11:13:24 +0300

    commit - racheli - splitFuncs

--- main.py
+++ main.py
@@ -34,3 +34,48 @@ def load_data(path):
 #com2g
 
 #next commit - racheli
+
+
+#split funcs:
+def my_train_test_split(DataFrameX, y=None, **kwargs):
+    # split a df to train and test, or train and validation, depend on the kwargs selection
+    if True in kwargs:
+        size = 0.18
+    else:
+        size = 0.15
+    if y is None:
+        y = DataFrameX['labels']
+        DataFrameX = DataFrameX.drop(['labels'], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(DataFrameX, y, test_size=size, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+# //when data includes labels (but it`s unnecessary):
+def my_train_test_split_labeled(DataFrameX, **kwargs):
+    # split a df to train and test, or train and validation, depend on the kwargs selection
+    if True in kwargs:
+        size = 0.18
+    else:
+        size = 0.15
+
+    X_train, X_test, y_train, y_test = train_test_split(DataFrameX.drop(['labels'], axis=1), DataFrameX['labels'],
+                                                        test_size=size, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+# merging df func
+def merge_dataFrame(A, B):
+    return A.append(B)
+
+
+def save_csv(df, path, fig):
+    try:
+        df.to_csv(path, sep=fig)
+        return True
+    except:
+        return False
+
+
+def open_csv(path):
+    df = pd.read_csv(path)
+    return df
