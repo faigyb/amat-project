040ecebc322d071f06f537bc2ea3cfe02c4bba3a
amatProject <amatproject2022@gmail.com>
2022-07-05 14:59:32 +0300

    change to new_image

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 import extractImages
 
-selected_classes = [1, 2, 4, 15, 18]
+selected_classes = [1, 2, 4, 14, 17]
 
-extractImages.load_all_data(r'C:\amatProject',selected_classes)
+# extractImages.load_all_data(r'C:\amatProject',selected_classes)
+# extractImages.add_our_pictures(r'C:\Users\biali\Pictures\Camera Roll',r'C:\amatProject\our_resized_images')
+# extractImages.add_one_image(r'C:\faigy bootcamp\basic-original.png',r'C:\amatProject\our_resized_images')
 
+#extractImages.create_labels_json()
