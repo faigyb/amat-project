e4577e4cab749d247381760e7adb9144df044c54
RaShahor <raracheli5400@gmail.com>
2022-07-07 16:31:56 +0300

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from tensorflow import keras
+from keras.datasets import cifar10
 
 import extractImages
 import visualize_DB
@@ -10,5 +12,12 @@ selected_classes = [1, 2, 4, 14, 17]
 # extractImages.add_one_image(r'C:\faigy bootcamp\basic-original.png',r'C:\amatProject\our_resized_images')
 
 #extractImages.create_labels_json()
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 visualize_DB.class_samples_number()
 
