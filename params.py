19cf3376e42ae993f0013bda1713231a04ebcf84
RaShahor <raracheli5400@gmail.com>
2022-07-12 11:01:47 +0300

    params add

--- params.py
+++ params.py
@@ -8,3 +8,5 @@ cifar10_to_execute=os.path.join(save_all_directory, 'cifar-10-batches-py')
 cifar100_to_execute=os.path.join(save_all_directory, 'cifar-100-python')
 our_images_directory=os.path.join(save_all_directory, 'our_images.csv')
 labels_json=os.path.join(save_all_directory, 'labels_names.json')
+train_size=0.15
+valid_size=0.18
