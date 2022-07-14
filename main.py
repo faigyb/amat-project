e0515d67be0ac517dc8032758d637adb1cfd7023
faigy <faigyb2002@gmail.com>
2022-07-14 18:24:54 +0300

    notes from ariel2

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import shutil
 import pandas as pd
 import os
 
+import params
+
 import extractImages
 import visualize_DB
 selected_classes = [1, 2, 4, 14, 17]
-extractImages.load_cifar100_data_into_CSV(r'C:\amatProject\cifar-100-python',[0,12])
+#extractImages.load_cifar100_data_into_CSV(r'C:\amatProject\cifar-100-python',[0,12])
 # extractImages.load_cifar10_data_into_CSV(r'C:\amatProject\cifar-10-batches-py')
 
 # extractImages.load_all_data(r'C:\amatProject',selected_classes)
@@ -15,8 +17,6 @@ extractImages.load_cifar100_data_into_CSV(r'C:\amatProject\cifar-100-python',[0,
 
 #extractImages.create_labels_json()
 # visualize_DB.class_samples_number()
-
-
-#
-# file_path = r'C:\amatProject\our_resized_images'
-# shutil.rmtree(file_path)
+import split_and_csv
+# split_and_csv.split_and_save(params.cifarCSV,params.save_all_directory)
+# visualize_DB.image_examples()
