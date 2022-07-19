8de7a5e6e4d6907efecc50e837820ecaed2f1049
RaShahor <raracheli5400@gmail.com>
2022-07-19 09:41:02 +0300

    make up racheli's changes

--- params.py
+++ params.py
@@ -1,10 +1,10 @@
 import os
 
-save_all_directory='../'
+save_all_directory = '../'
 
-images_directory=os.path.join(save_all_directory, 'images')
-cifarCSV=os.path.join(save_all_directory, 'CIFAR-10.csv')
-cifar10_to_execute=os.path.join(save_all_directory, 'cifar-10-batches-py')
-cifar100_to_execute=os.path.join(save_all_directory, 'cifar-100-python')
-our_images_directory=os.path.join(save_all_directory, 'our_images.csv')
-labels_json=os.path.join(save_all_directory, 'labels_names.json')
+images_directory = os.path.join(save_all_directory, 'images')
+cifarCSV = os.path.join(save_all_directory, 'CIFAR-10.csv')
+cifar10_to_execute = os.path.join(save_all_directory, 'cifar-10-batches-py')
+cifar100_to_execute = os.path.join(save_all_directory, 'cifar-100-python')
+our_images_directory = os.path.join(save_all_directory, 'our_images.csv')
+labels_json = os.path.join(save_all_directory, 'labels_names.json')
