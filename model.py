da8696b70e3371a51cf15e74038d8ae1c0028c10
RaShahor <raracheli5400@gmail.com>
2022-07-20 16:42:09 +0300

    prediction with thresh - 3 -important fix!

--- model.py
+++ model.py
@@ -12,13 +12,14 @@ import create_data
 
 
 def my_load_model():
-    model_path=params.model_path
-    model = load_model(model_path)
+    model_path = params.model_path
+    model = load_model(r"C:\Users\1\Desktop\imagesProject2\keras_cifar10_trained_model_dataAugmentation2.h5")
     return model
 def load_json(file):
     with open(file, 'r') as f:
-        List = json.load(f) #load classes names
-        return  List
+        list = json.load(f)
+        print(type(list))#load classes names
+        return list
 def load_labels():
     with open(params.labels_json, 'r') as f:
         classes_names = json.load(f) #load classes names
@@ -34,24 +35,19 @@ def predict(image_path,model):
     image = [image]
     image = np.asarray(image)
     image_pred = model.predict(image)[0]
-
+    print (image_pred)
     ind=np.argmax(image_pred)
     return labels[str(ind)]
-def predict_pro(img):
-    model = my_load_model()
+def predict_pro(img,model):
+    #model = my_load_model()
     labels = load_json(params.labels_json)
     threshes = load_json(params.thresh_json)
-
     image = Image.open(img)
     image = np.asarray(image)
+    image = image.astype('float32')
+    image /= 255
     image = [image]
     image = np.asarray(image)
-    image=image.astype('float32')
-
-    mean_image =image.mean(axis=(0, 1, 2), keepdims=True)
-    std_image = image.std(axis=(0, 1, 2), keepdims=True)
-
-    image = (image - mean_image) / std_image
     image_pred_prob = model.predict(image)
     print(f"max: {np.mean(image_pred_prob)},\nargmax: {np.argmax(image_pred_prob)}\nmaxPro: {np.max(image_pred_prob)}\nthr: {threshes}")
 
@@ -60,4 +56,5 @@ def predict_pro(img):
     else:ind='none'
     print(f"probs: {image_pred_prob}\nind: {ind}\n {type(image_pred_prob[0][0])}")
     return ind
-
+create_data.define_thresh_json()
+print(predict_pro(r"C:\Users\1\Pictures\amat_images\fish_2.png",my_load_model()))
