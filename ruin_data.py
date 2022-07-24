f3c5797c359e8ed541ccac24cdf744b6773d0314
faigy <faigyb2002@gmail.com>
2022-07-24 17:52:41 +0300

    ruin data

--- ruin_data.py
+++ ruin_data.py
@@ -7,10 +7,8 @@ import os
 def ruin_data(train_csv_path:str):
     print(train_csv_path)
     df = pd.read_csv(train_csv_path)
-    print(df.head())
     labels=df['labels']
     shuffle(labels[:len(labels)//10])
-    print(labels)
     df['labels']=labels
     #save to csv ruin_TestData
     ruined_TrainData_csv=os.path.join(params.save_all_directory, 'ruined_TrainData.csv')
