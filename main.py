38d240f4211969bc3a086da5731fdbcd49a719ad
faigy <faigyb2002@gmail.com>
2022-07-27 07:52:22 +0300

    ruin data

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import shutil
-
+import params
 import pandas as pd
 import os
-
-import extractImages
-import visualize_DB
+import visualize_DB,extract_cifar,create_data,split_and_csv,ruin_data
+import numpy as np
 selected_classes = [1, 2, 4, 14, 17]
-# extractImages.load_cifar100_data_into_CSV(r'C:\amatProject\cifar-100-python',[0,12])
-# extractImages.load_cifar10_data_into_CSV(r'C:\amatProject\cifar-10-batches-py')
+# extract_cifar.load_all_data(r'C:\amatProject',selected_classes)
 
-# extractImages.load_all_data(r'C:\amatProject',selected_classes)
+# create_data.create_labels_json()
+
+#'we now would like to see our data distrebution.
+# visualize_DB.class_samples_number()
+
+#now we need to divide our data into train test and validation
+# split_and_csv.split_and_save(params.cifarCSV,params.save_all_directory)
+
+#we can now see how the data distrebutes between the train test and validation
+# visualize_DB.pie_chart_split(os.path.join(params.save_all_directory,'TrainData.csv'),os.path.join(params.save_all_directory,'ValidationData.csv'),os.path.join(params.save_all_directory,'TestData.csv'))
+
 # extractImages.add_our_pictures(r'C:\Users\biali\Pictures\Camera Roll',r'C:\amatProject\our_resized_images')
 # extractImages.add_one_image(r'C:\faigy bootcamp\basic-original.png',r'C:\amatProject\our_resized_images')
 
-#extractImages.create_labels_json()
-# visualize_DB.class_samples_number()
+#now we take all the data (train, test and validation and insert it (after normolized) to npz file that we can upload to google drive
+# create_data.savez_images()
+# ruin_data.ruin_data(os.path.join(params.save_all_directory, 'TrainData.csv'))
+# ruin_data.savez_images()
 
-
-#
-# file_path = r'C:\amatProject\our_resized_images'
-# shutil.rmtree(file_path)
--- ruin_data.py
+++ ruin_data.py
@@ -3,12 +3,24 @@ import pandas as pd
 import numpy as np
 from random import shuffle
 import os
+import create_data
 
 def ruin_data(train_csv_path:str):
     df = pd.read_csv(train_csv_path)
+    print(df)
     labels=df['labels']
     shuffle(labels[:len(labels)//10])
     df['labels']=labels
     #save to csv ruin_TestData
     ruined_TrainData_csv=os.path.join(params.save_all_directory, 'ruined_TrainData.csv')
-    df.to_csv(ruined_TrainData_csv)
+    df.to_csv(ruined_TrainData_csv,index=False)
+
+
+def savez_images():
+    x_train,y_train=create_data.create_dataset(params.save_all_directory+"ruined_TrainData.csv")
+    print("train")
+    x_test,y_test=create_data.create_dataset(params.save_all_directory+"TestData.csv")
+    print("test")
+    x_validation,y_validation=create_data.create_dataset(params.save_all_directory+"ValidationData.csv")
+    print("validation")
+    np.savez(params.save_all_directory+"cfar10_ruined_data.npz", train=x_train, ytrain=y_train, test=x_test, ytest=y_test,validation=x_validation, yvalidation=y_validation)
